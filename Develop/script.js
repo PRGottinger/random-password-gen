fd347fff1f212b56e39b882e227ba79dd2852696
Paul Gottinger <pgottinger@outlook.com>
2022-01-12 19:52:45 -0600

    commit 2

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,21 @@
 // Assignment code here
 
-//hello
+//hello!!!
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  console.log("You clicked the button!");
+
+  // 1. prompt the user for the password criteria
+  // a. password length 8 - 128
+  // b. lowercasse, uppercase, numbers, special characters
+  // 2. validate the input.
+  // 3. Generate password based on criteria 
+
+  return "Generated password will go here!"
+}
 
 // Write password to the #password input
 function writePassword() {
