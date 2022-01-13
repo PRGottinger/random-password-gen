843ed58fd39fad962805fe721e7eddf8b50280eb
Paul Gottinger <pgottinger@outlook.com>
2022-01-12 23:18:17 -0600

    third commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,44 @@
 // Assignment code here
 
-//hello!!!
+// //Function to get password inputs
+// var passwordCharacters = function () {
+
+// }
+
+// console.log(characterNumber);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+buttonEl.addEventListener("click", function () {
+  alert("button clicked");
+});
+
 function generatePassword() {
-  console.log("You clicked the button!");
+  // console.log("You clicked the button!");
+  // window.alert("Hello! Please select password length");
+
+  // let lower = "abcdefghijklmnopqrstuvwxyz";
+  // let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // lower = lower.split("");
+  // upper = upper.split("");
+  // console.log(lower);
+  // for (let i = 0; i < str.length; i++) {
+  //   console.log(str.charCodeAt(i), str[i]);
+  // }
+
+  //let length = prompt('enter a length')
 
   // 1. prompt the user for the password criteria
   // a. password length 8 - 128
   // b. lowercasse, uppercase, numbers, special characters
   // 2. validate the input.
-  // 3. Generate password based on criteria 
+  // 3. Generate password based on criteria
 
-  return "Generated password will go here!"
+  return "Generated password will go here!";
 }
+
+generatePassword();
 
 // Write password to the #password input
 function writePassword() {
